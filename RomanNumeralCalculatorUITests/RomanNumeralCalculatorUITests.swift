eb314f1f2411c624efc9809a78795f8ebd264aef
Andreas Bjerga <andreas.bjerga@proplan.no>
2017-06-08 14:33:49 +0200

    Add toRoman unit tests

--- RomanNumeralCalculatorUITests/RomanNumeralCalculatorUITests.swift
+++ RomanNumeralCalculatorUITests/RomanNumeralCalculatorUITests.swift
@@ -2,8 +2,8 @@
 //  RomanNumeralCalculatorUITests.swift
 //  RomanNumeralCalculatorUITests
 //
-//  Created by Jarrod Parkes on 11/29/16.
-//  Copyright © 2016 buddybuild. All rights reserved.
+//  Created by Andreas Bjerga on 08.06.2017.
+//  Copyright © 2017 buddybuild. All rights reserved.
 //
 
 import XCTest
